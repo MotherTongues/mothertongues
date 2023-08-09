8c203e209e1f72408f68a866b9684327d838e4eb
Aidan Pine <hello@aidanpine.ca>
2023-08-09 13:32:17 -0700

    feat(cli): add output path argument

--- mothertongues/cli.py
+++ mothertongues/cli.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 import typer
+from loguru import logger
 
 from mothertongues.config.models import MTDConfiguration
 from mothertongues.dictionary import MTDictionary
@@ -20,23 +21,31 @@ def export(
     language_config_path: Path = typer.Argument(
         default=None, exists=True, file_okay=True, dir_okay=False, readable=True
     ),
+    output_directory: Path = typer.Argument(
+        default=None, exists=True, file_okay=False, dir_okay=True
+    ),
     single_file: bool = typer.Option(default=True),
 ):
     config = MTDConfiguration(**load_mtd_configuration(language_config_path))
     dictionary = MTDictionary(config)
+
     if single_file:
         output = dictionary.export()
-        with open("dictionary_data.json", "w", encoding="utf8") as f:
+        logger.info(
+            f"Writing dictionary data file to {(output_directory / 'dictionary_data.json')}"
+        )
+        with open(output_directory / "dictionary_data.json", "w", encoding="utf8") as f:
             json.dump(output, f)
     else:
         config, data, l1_index, l2_index = dictionary.export(combine=False)
-        with open("config.json", "w", encoding="utf8") as f:
+        logger.info(f"Writing config, data, and index files to {(output_directory)}")
+        with open(output_directory / "config.json", "w", encoding="utf8") as f:
             json.dump(config, f)
-        with open("data_hash.json", "w", encoding="utf8") as f:
+        with open(output_directory / "data_hash.json", "w", encoding="utf8") as f:
             json.dump(data, f)
-        with open("l1_index.json", "w", encoding="utf8") as f:
+        with open(output_directory / "l1_index.json", "w", encoding="utf8") as f:
             json.dump(l1_index, f)
-        with open("l2_index.json", "w", encoding="utf8") as f:
+        with open(output_directory / "l2_index.json", "w", encoding="utf8") as f:
             json.dump(l2_index, f)
 
 
